427782702b35518a95a9d44ee7c6709210653ef3
YifengGuo <gyf940517@gmail.com>
2018-11-02 10:06:17 +0800

    update compatibility problem

--- src/main/java/com/yifeng/restclient/test/InsertMockEventDataToES.java
+++ src/main/java/com/yifeng/restclient/test/InsertMockEventDataToES.java
@@ -28,7 +28,7 @@ import static com.yifeng.restclient.config.DatasourceConstant.ES_HOSTS;
 public class InsertMockEventDataToES {
     static RestHighLevelClient restClient;
     static final String[] FIELDS = {"﻿standby_number5", "standby_char18", "receive_time", "standby_char19", "event_level", "file_name", "standby_char26", "occur_time", "sa_sp_ap_da_dp", "file_size", "src_geo", "source_host", "event_name", "standby_number6", "src_longitude_latitude_name", "standby_char36", "dev_city", "original_log", "id", "src_city", "src_filepath", "standby_char13", "event_type", "dev_geo", "standby_number7", "first_time", "src_address", "sa_da", "user_account", "file_type", "rule_name", "dev_longitude_latitude_name", "dev_address", "standby_number8", "vendor", "event_id", "user_name", "standby_char27", "product", "mail_subect", "send_mail", "receive_mail", "blind_carbon_copy", "standby_number9"};
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         initial();
         String csvFile = "/Users/guoyifeng/Downloads/query_result.csv";
         BufferedReader br = null;
@@ -49,16 +49,20 @@ public class InsertMockEventDataToES {
                 boolean exists = restClient.indices().exists(new GetIndexRequest().indices(curIndexName), RequestOptions.DEFAULT);
                 if (!exists) {
                     // index does not exist
-                    CreateIndexRequest request = new CreateIndexRequest(curIndexName);
-                    request.settings(Settings.builder()
-                            .put("index.number_of_shards", 1)
-                            .put("index.number_of_replicas", 0)
-                    );
-                    CreateIndexResponse createIndexResponse = restClient.indices().create(request, RequestOptions.DEFAULT);
-                    // put mappings for the index
                     Runtime rt = Runtime.getRuntime();
-                    String command = "curl -X PUT \"http://172.16.150.149:29200/" + curIndexName + "\" -H 'Content-Type: application/json' -d' {\"mappings\":{\"event\":{\"properties\":{\"standby_number5\":{\"type\":\"integer\"},\"standby_char18\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"receive_time\":{\"include_in_all\":false,\"format\":\"dateOptionalTime||epoch_millis\",\"type\":\"date\"},\"standby_char19\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_level\":{\"type\":\"integer\"},\"file_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char26\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"occur_time\":{\"include_in_all\":false,\"format\":\"dateOptionalTime||epoch_millis\",\"type\":\"date\"},\"sa_sp_ap_da_dp\":{\"type\":\"string\"},\"file_size\":{\"type\":\"long\"},\"src_geo\":{\"type\":\"geo_point\"},\"source_host\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_number6\":{\"type\":\"integer\"},\"src_longitude_latitude_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char36\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_city\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"original_log\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"id\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"src_city\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"src_filepath\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char13\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_type\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_geo\":{\"type\":\"geo_point\"},\"standby_number7\":{\"type\":\"integer\"},\"first_time\":{\"include_in_all\":false,\"format\":\"dateOptionalTime||epoch_millis\",\"type\":\"date\"},\"src_address\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"sa_da\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"user_account\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"file_type\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"rule_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_longitude_latitude_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_address\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_number8\":{\"type\":\"integer\"},\"vendor\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_id\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"user_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char27\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"product\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"mail_subect\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"send_mail\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"receive_mail\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"blind_carbon_copy\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_number9\":{\"type\":\"integer\"}}}}}' ";
-                    rt.exec(command);
+                    String indexCommand = "curl -X PUT \"http://172.16.150.149:29200/" + curIndexName + "\"-H 'Content-Type: application/json' -d' { \"settings\" : { \"index\" : { \"number_of_shards\" : 1, \"number_of_replicas\" : 0 } } } '";
+                    rt.exec(indexCommand);
+
+                    //  6.0 API does not support es older version
+//                    CreateIndexRequest request = new CreateIndexRequest(curIndexName);
+//                    request.settings(Settings.builder()
+//                            .put("index.number_of_shards", 1)
+//                            .put("index.number_of_replicas", 0)
+//                    );
+//                    restClient.indices().create(request, RequestOptions.DEFAULT);
+                    // put mappings for the index
+                    String mappingsCommand = "curl -X PUT \"http://172.16.150.149:29200/" + curIndexName + "\" -H 'Content-Type: application/json' -d' {\"mappings\":{\"event\":{\"properties\":{\"standby_number5\":{\"type\":\"integer\"},\"standby_char18\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"receive_time\":{\"include_in_all\":false,\"format\":\"dateOptionalTime||epoch_millis\",\"type\":\"date\"},\"standby_char19\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_level\":{\"type\":\"integer\"},\"file_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char26\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"occur_time\":{\"include_in_all\":false,\"format\":\"dateOptionalTime||epoch_millis\",\"type\":\"date\"},\"sa_sp_ap_da_dp\":{\"type\":\"string\"},\"file_size\":{\"type\":\"long\"},\"src_geo\":{\"type\":\"geo_point\"},\"source_host\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_number6\":{\"type\":\"integer\"},\"src_longitude_latitude_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char36\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_city\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"original_log\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"id\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"src_city\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"src_filepath\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char13\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_type\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_geo\":{\"type\":\"geo_point\"},\"standby_number7\":{\"type\":\"integer\"},\"first_time\":{\"include_in_all\":false,\"format\":\"dateOptionalTime||epoch_millis\",\"type\":\"date\"},\"src_address\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"sa_da\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"user_account\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"file_type\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"rule_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_longitude_latitude_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"dev_address\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_number8\":{\"type\":\"integer\"},\"vendor\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"event_id\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"user_name\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_char27\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"product\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"mail_subect\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"send_mail\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"receive_mail\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"blind_carbon_copy\":{\"type\":\"string\",\"index\":\"not_analyzed\"},\"standby_number9\":{\"type\":\"integer\"}}}}}' ";
+                    rt.exec(mappingsCommand);
 
                     // index document to es
                     Map<String, Object> curMap = doc(tokens);
@@ -86,7 +90,7 @@ public class InsertMockEventDataToES {
         }
     }
 
-    private static void initial() throws Exception {
+    private static void initial() {
         restClient = new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost(ES_HOSTS, 29200, "http")));
