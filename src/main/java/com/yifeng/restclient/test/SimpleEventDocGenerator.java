10b390e456dc7289a3bb194edc8777e2bf43b641
YifengGuo <gyf940517@gmail.com>
2019-11-05 15:35:34 +0800

    test

--- src/main/java/com/yifeng/restclient/test/SimpleEventDocGenerator.java
+++ src/main/java/com/yifeng/restclient/test/SimpleEventDocGenerator.java
@@ -21,7 +21,7 @@ public class SimpleEventDocGenerator {
     private static final String INDEX_PREFIX = "event_";
     private static final String TYPE = "event";
 
-    private static final String ES_HOSTS = "172.16.150.184";
+    private static final String ES_HOSTS = "172.16.150.60";
     private static final int PORTS = 9200;
     private static final String HTTP_SCHEMA = "http";
 
@@ -29,18 +29,48 @@ public class SimpleEventDocGenerator {
     static RestHighLevelClient restClient;
     static RestClient lowLevelClient;
     static final String[] FIELDS = {"user_name", "file_name", "file_size", "event_id", "occur_time", "file_type"};
-    static final String[] OLD_USERS = {"Y##FENG", "new_user_1"};
+    static final String[] OLD_USERS = {"YIFENG", "new_user_1"};
+    static final String[] BLACKLIST_USERS = {"blacklist_user_1", "blacklist_user_2"};
     static final String[] FILE_POSTFIX = {".doc", ".py", ".exe"};
     static final String[] DEV_IP = {"172.16.150.60", "172.16.150.189", "172.16.150.123", "172.16.150.172"};
     static final String[] SRC_IP = {"172.16.150.106", "172.16.150.107", "172.16.150.108", "172.16.150.109"};
     static SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 2; ++i) {
-            indexDoc(initDoc(1569292140000L));
+//        for (int i = 0; i < 2; ++i) {
+//            indexDoc(initDoc(1569292140000L));
+//        }
+
+        for (int i = 0; i < 10; ++i) {
+            indexDoc(initBlacklistDoc(1569513600000L));
         }
+
+
 //        for (int i = 0; i < 5; i++) {
 //            indexDoc(initFlagDoc(1569315390000L));
 //        }
+    }
+
+    public static JSONObject initBlacklistDoc(long time) {
+        JSONObject obj = new JSONObject();
+        Long now;
+        if (time == 0L) {
+            now = System.currentTimeMillis();
+        } else {
+            now = time;
+        }
+
+        obj.put("src_city", "Intranet");
+        obj.put("dev_city", "Intranet");
+        obj.put("user_name", BLACKLIST_USERS[getRandomIndex(OLD_USERS.length)]);
+        String postfix = FILE_POSTFIX[getRandomIndex(FILE_POSTFIX.length)];
+        obj.put("file_name", getAlphaNumericString(6) + postfix);
+        obj.put("receive_time", now);
+        obj.put("dev_address", DEV_IP[getRandomIndex(DEV_IP.length)]);
+        obj.put("file_size", ThreadLocalRandom.current().nextInt(2000));
+        obj.put("occur_time", now);
+        obj.put("file_type", postfix);
+        obj.put("src_address", SRC_IP[getRandomIndex(SRC_IP.length)]);
+        return obj;
     }
 
     public static JSONObject initDoc(long time) {
--- src/main/java/com/yifeng/restclient/test/TestElasticsearchConnection.java
+++ src/main/java/com/yifeng/restclient/test/TestElasticsearchConnection.java
@@ -28,19 +28,20 @@ public class TestElasticsearchConnection {
         String id = "OEH0380";
         String configId = "da272dbd-a650-42ce-9ff1-38c350eaf4d9";
         boolean isEntity = false;
-        BoolQueryBuilder qb = boolQuery().must(QueryBuilders.boolQuery()
-                .should(matchQuery("id", id))
-                .should(matchQuery("id", id.toLowerCase())));
-        if (isEntity) {
-            qb = qb.must(matchQuery("entity_config_id", configId));
-        }
+//        BoolQueryBuilder qb = boolQuery().must(QueryBuilders.boolQuery()
+//                .should(matchQuery("id", id))
+//                .should(matchQuery("id", id.toLowerCase())));
+//        if (isEntity) {
+//            qb = qb.must(matchQuery("entity_config_id", configId));
+//        }
+        String queryString = "user_name:zongzhengjie AND occur_time:>=1571313010603 AND occur_time:<=1571313020603 AND src_geo:(30.5728,104.0668 OR 32.06167,118.77778) AND (_exists_:src_geo AND NOT src_geo:*anet* AND src_geo:*,* AND user_name:zongzhengjie)";
         ElasticsearchConnection connection = new ElasticsearchConnection();
         try {
-            connection.connect("172.16.150.149", 29200, "http");
-            response = connection.client().search(new SearchRequest("ueba_settings")
-                    .types("user_info")
+            connection.connect("172.16.150.60", 9200, "http");
+            response = connection.client().search(new SearchRequest("event_201910*")
+                    .types("event")
                     .source(new SearchSourceBuilder()
-                            .query(qb)));
+                            .query(QueryBuilders.queryStringQuery(queryString))));
             System.out.println(response.getHits().getHits()[0].getSourceAsMap());
         } catch (IOException e) {
             e.printStackTrace();
